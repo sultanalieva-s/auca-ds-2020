afe2039998624f0135bc4bfa2d0c860771313ec4
Saadat <saadatssu@gmail.com>
2020-09-12 00:06:37 +0600

    set proper name for problems folders

--- lab-02/p01/main.cpp
+++ lab-02/p01/main.cpp
@@ -1,42 +1,16 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int inp;
-    while(cin >> inp && inp != 0)
-    {
-        if(inp == 0)
-        break;
+    int articles, impactFactor;
+    cin >> articles >> impactFactor;
 
-        int sumOfInp = 0;
-        int t = inp;
+    //factor = total of citations / articles
+    // citations = articles * factor
 
-        while(t > 0)
-            {
-                sumOfInp += t%10;
-                t /= 10;
-            }
+    int scientists = ceil(articles * (double)(impactFactor - 0.99));
 
-        int startNum = 11;
-
-        while(true)
-        {
-            int production = startNum * inp;
-            int sumOfProd = 0;
-            while(production > 0)
-            {
-                sumOfProd += production%10;
-                production /= 10;
-            }
-
-            if(sumOfInp == sumOfProd){
-                cout<< startNum << endl;
-                break;
-            }else{
-                startNum++;
-            }
-        }
-    }
-    return 0;
+    cout << scientists << endl;
 }
