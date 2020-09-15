dedf2736633e153b63fc8da417d4e98af83e48e1
Saadat <saadatssu@gmail.com>
2020-09-15 19:09:20 +0600

    lab02 p05 wrong answer

--- lab-02/p05/main.cpp
+++ lab-02/p05/main.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 int main()
 {
-    int a, b;
-    cin >> a >> b;
+    long long a, b;
 
-    cout << abs(a - b) << endl;
+    while (cin >> a, b)
+    {
+        cout << abs(a - b) << endl;
+    }
 
 }
