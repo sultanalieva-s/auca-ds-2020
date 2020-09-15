45b2835020427ac38422b631a57236761a8539ad
Saadat <saadatssu@gmail.com>
2020-09-15 18:56:48 +0600

    lab02 p04 accepted

---
+++ lab-02/p04/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    getline(cin, name);
+    cout << "Thank you, " << name << ", and farewell!" << endl;
+}
