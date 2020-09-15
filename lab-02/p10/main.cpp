30a9630104ae0c0af7970cf73717734754717a1f
Saadat <saadatssu@gmail.com>
2020-09-15 23:14:30 +0600

    lab02 p10 wrong answer

---
+++ lab-02/p10/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int easy, med, hard, total;
+
+    cin >> easy >> med >> hard >> total;
+    
+    if(easy == 0 || med == 0 || hard == 0 || easy+med+hard < total)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        cout << "YES";
+    }
+    
+
+}
