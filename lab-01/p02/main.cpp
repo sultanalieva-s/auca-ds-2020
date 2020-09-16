3b3cb5fd3f27156aef0283a2f48149faa74a6fad
Saadat <saadatssu@gmail.com>
2020-09-16 12:11:43 +0600

    lab-02 p02 finished

---
+++ lab-01/p02/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+const int kNameWidth = 25;
+const int kAverageWidth = 40;
+
+int main()
+{
+    cout << setfill('.')<<fixed<<setprecision(2);
+    for (string name; cin >> name;)
+    {
+        double sum = 0;
+        int nGrades = 0;
+        for (int grade; cin >> grade;)
+        {
+            sum += grade;
+            nGrades++;
+        }
+        if (nGrades != 0)
+        {
+            cout << left << setw(kNameWidth)<< name << "|" << right<< setw(kAverageWidth) << sum / nGrades << endl;
+        }
+        else
+        {
+            cout << left << setw(kNameWidth) << name << "|"
+                 << right <<setw(kAverageWidth) << "no data" << endl;
+        }
+
+        if (cin.fail())
+        {
+            cin.clear();
+        };
+    }
+}
