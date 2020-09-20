9d38a3b58b5811a2c82e26241d5c961072126914
Saadat <saadatssu@gmail.com>
2020-09-20 18:33:38 +0600

    lab-02 p36 hothike wrong answer

---
+++ lab-02/p36/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> days;
+    vector<int> maximums;
+    while (n != 0)
+    {
+        int t;
+        cin >> t;
+        days.push_back(t);
+        n--;
+    }
+
+    for (int i = 0; i < days.size() - 2; i++)
+    {
+        maximums.push_back(max(days[i], days[i + 2]));
+    }
+    int min = maximums[0];
+    int index = 0;
+    for (int i = 0; i < maximums.size(); i++)
+    {
+        if (min > maximums[i])
+        {
+            min = maximums[i];
+            index = 0;
+        }
+    }
+    index++;
+
+    cout << index << " " << min << endl;
+}
