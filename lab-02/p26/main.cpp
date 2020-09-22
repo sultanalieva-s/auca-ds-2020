08b5246cfd50782d2a6ae34785e9651e054750d5
Saadat <saadatssu@gmail.com>
2020-09-23 01:00:56 +0600

    lab-02 p26 wrong answer test2

---
+++ lab-02/p26/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t != 0)
+    {
+        int p, r, f;
+
+        cin >> p >> r >> f;
+        int count = 0;
+
+        while (p <= f)
+        {
+            count++;
+            p *= r;
+        }
+        cout << count << endl;
+
+        t--;
+    }
+}
