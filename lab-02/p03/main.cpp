0047314b57be0b891d291b005b71d5cb70eaf947
Saadat <saadatssu@gmail.com>
2020-10-01 21:50:20 +0600

    lab-02 p03 accepted

--- lab-02/p03/main.cpp
+++ lab-02/p03/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
     double n;
-    cin>>n;
-    double o = 1000 * (double(5280)/4854) * n;
-    cout<<ceil(o)<<endl;
+    cin >> n;
+    double o = 0.5 + 1000.0 * (double(5280) / double(4854)) * n;
+    cout << int(o) << endl;
 }
