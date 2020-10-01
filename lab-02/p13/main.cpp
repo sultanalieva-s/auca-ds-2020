b3e675cf35d1d0c422915ff69f603e63ed318b00
Saadat <saadatssu@gmail.com>
2020-10-02 01:15:18 +0600

    lab-03 p13 wrong answer test 24

---
+++ lab-02/p13/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    //progression: x + n*y = n;
+    // n = -x / (y -1)
+    if(y == 1)
+    {
+        cout<<"ALL GOOD"<<endl;
+        return 0;
+    }
+    double n = double(-x) / (double)(y - 1);
+    if(n - int(n) == 0)
+    {
+        cout << n << endl;
+    }
+    else
+    {
+        cout<<fixed<<setprecision(9)<< n<< endl;
+    }
+    
+    
+}
