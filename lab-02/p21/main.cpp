1508721a3b4f1758403fbb5bcad957dd816f86fe
Saadat <saadatssu@gmail.com>
2020-10-06 15:08:57 +0600

    lab-02 p21 ACCEPTED

--- lab-02/p21/main.cpp
+++ lab-02/p21/main.cpp
@@ -6,13 +6,13 @@ int main()
 {
     int t;
     cin >> t;
-    int min = INT8_MAX;
+    int min = -1;
     int index;
     for(int i = 0; i < t; i++)
     {
         int in;
         cin>>in;
-        if(in < min)
+        if(min == -1 || min > in)
         {
             min = in;
             index= i;
