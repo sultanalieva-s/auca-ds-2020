c1ad813543dddb1139f722e736263fa2a2e5a258
Saadat <saadatssu@gmail.com>
2020-10-14 20:46:31 +0600

    lab-02 p13 accepted

--- lab-02/p13/main.cpp
+++ lab-02/p13/main.cpp
@@ -8,13 +8,18 @@ int main()
     cin >> x >> y;
     //progression: x + n*y = n;
     // n = -x / (y -1)
-    double n = double(-x) / (double)(y - 1);
-    if (n - int(n) == 0)
+
+    if (x == 0 && y == 1)
     {
-        cout << n << endl;
+        cout << "ALL GOOD" << endl;
+    }
+    else if (y == 1)
+    {
+        cout << "IMPOSSIBLE" << endl;
     }
     else
     {
+        double n = double(-x) / (double)(y - 1);
         cout << fixed << setprecision(9) << n << endl;
     }
 }
