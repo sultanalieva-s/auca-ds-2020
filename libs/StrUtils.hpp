6db25b0fb4b5a3eb53714eddffaf7ac028363a9b
Saadat <saadatssu@gmail.com>
2020-11-01 20:15:03 +0600

    track libs folder

---
+++ libs/StrUtils.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+#include <vector>
+
+std::vector<std::string> split(const std::string &line);
+
+std::string join(const std::vector<std::string> &v);
---
+++ libs/join.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+string join(const vector<string> &v)
+{
+    string r;
+
+    if (!v.empty())
+    {
+        r = v[0];
+
+        for (size_t i = 1; i < v.size(); i++)
+        {
+            r += " ";
+            r += v[i];
+        }
+    }
+    return r;
+}
